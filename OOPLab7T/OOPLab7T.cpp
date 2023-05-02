30452dd3d94ee6accb0b7e0444f9fdcd1a77388c
PozhohaVitalii <112852134+PozhohaVitalii@users.noreply.github.com>
2023-05-02 11:06:56 +0300

    sasas

--- OOPLab7T/OOPLab7T.cpp
+++ OOPLab7T/OOPLab7T.cpp
@@ -32,7 +32,7 @@ public:
     int getCarId() const { return id_car; }
     string getRaceStatus() const { return race_status; }
 };
-/*
+
 class Autobaza {
 private:
     vector<Dyspetcher> dyspetchers;
@@ -41,94 +41,9 @@ private:
     vector<Car> cars;
     vector<Race> races;
 public:
-    // methods to add objects to the vectors
-    void addDyspetcher(const Dyspetcher& d) { dyspetchers.push_back(d); }
-    void addTrip(const Trip& t) { trips.push_back(t); }
-    void addDriver(const Driver& d) { drivers.push_back(d); }
-    void addCar(const Car& c) { cars.push_back(c); }
-    void addRace(const Race& r) { races.push_back(r); }
-
-    // method to get a driver by id
-    Driver getDriverById(int id) const {
-        for (const Driver& d : drivers) {
-            if (d.getId() == id) {
-                return d;
-            }
-        }
-        throw runtime_error("Driver not found");
-    }
-
-    // method to get a car by id
-    Car getCarById(int id) const {
-        for (const Car& c : cars) {
-            if (c.getId() == id) {
-                return c;
-            }
-        }
-        throw runtime_error("Car not found");
-    }
-
-    // method to find available cars
-    vector<Car> getAvailableCars() const {
-        vector<Car> available_cars;
-        for (const Car& c : cars) {
-            if (c.getCarStatus() == "Available") {
-                available_cars.push_back(c);
-            }
-        }
-        return available_cars;
-    }
-
-    // method to assign a driver and car to a race
-    void assignDriverAndCarToRace(int race_id, int driver_id, int car_id) {
-        for (Race& r : races) {
-            if (r.getId() == race_id) {
-                r = Race(r.getId(), r.getTripId(), r.getDyspetcherId(), driver_id, car_id, "Assigned");
-                break;
-            }
-        }
-    }
-
-    // method for a driver to request a repair
-    void requestRepair(int driver_id) {
-        for (Driver& d : drivers) {
-            if (d.getId() == driver_id) {
-                d = Driver(d.getId(), d.getFirstName(), d.getLastName(), d.getSex(), d.getExperience(), "Repair Requested");
-                break;
-            }
-        }
-    }
-
-    // method for a dispatcher to suspend a driver
-    void suspendDriver(int driver_id) {
-        for (Driver& d : drivers) {
-            if (d.getId() == driver_id) {
-                d = Driver(d.getId(), d.getFirstName(), d.getLastName(), d.getSex(), d.getExperience(), "Suspended");
-                break;
-            }
-        }
-    }
-
-    // method for a driver to update the race status and car mileage
-    void updateRaceStatusAndCarMileage(int driver_id, int race_id, string race_status, int car_mileage) {
-        for (Race& r : races) {
-            if (r.getId() == race_id && r.getDriverId() == driver_id) {
-                // update race status
-                r = Race(r.getId(), r.getTripId(), r.getDyspetcherId(), driver_id, r.getCarId(), race_status);
-                // update car mileage
-                for (Car& c : cars) {
-                    if (c.getId() == r.getCarId()) {
-                        c = Car(c.getId(), c.getMake(), c.getModel(), c.getYear(), c.getColor(),
-                            c.getNumberOfPassangers(), car_mileage, c.getCarStatus());
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-    }
+  
 };
-*/
+
 
 int main()
 {
